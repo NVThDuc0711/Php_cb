821d12dd8edefaa02c91cce04769ae763749b6a8
Nguyễn Văn Thành Đức <vanthanhduc2000@gmail.com>
2022-11-15 15:17:38 +0700

    type data

--- module/ex04.php
+++ module/ex04.php
@@ -6,4 +6,34 @@ var_dump($age);
 // ép kiểu 
 $age = (int)$age;
 var_dump($age);
-// kiểu dữ liệu boolean
+
+echo '<hr>';
+// 2. kiểu dữ liệu boolean
+$bool = 0;
+var_dump($bool);
+//ép kiểu 
+$bool = (bool)$bool ; 
+var_dump($bool); // nếu 0 hoặc rỗng sẽ là false còn ngược lại là true
+echo '<hr>';
+// 3 . Kiểu dữ liệu string 
+$str = 'Nguyễn Văn Thành Đức ';
+var_dump($str);
+echo '<hr>';
+// 4. NULL 
+$total = NULL ; 
+var_dump($total);
+
+$checkNull = is_null($total);
+var_dump($checkNull);
+echo '<hr>';
+// 5. Mảng 
+$Arr = ['1'];
+var_dump($Arr);
+// 6 . Object
+$Obj = [
+    'Duc'
+];
+$Obj=(Object)$Obj;
+var_dump($Obj);
+$Customer = new $Obj;
+var_dump($Customer);
